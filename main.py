fe670096178bda0b1a01a1616a7266aef0796739
Andrew Stepin <andrew.stepin@gmail.com>
2024-07-05 22:14:59 +0200

    major bump
    1. removed cpickle as deprecated
    2. tokenizer with transformers
    3. HDF5 for faster io
    4. support for pre-trained embeds

--- main.py
+++ main.py
@@ -1,191 +1,205 @@
-from nltk.tokenize import word_tokenize as tokenize
-import json, config, nltk, itertools
+import json
+import itertools
 from os import path
-import numpy as np
-import cPickle as pickle
+from typing import List, Tuple, Dict
 
-WHITELIST = '0123456789abcdefghijklmnopqrstuvwxyz'
+import numpy as np
+import pandas as pd
+import pickle
+import nltk
+from nltk.tokenize import word_tokenize
+from gensim.models import KeyedVectors
+from transformers import AutoTokenizer, AutoModel
+import h5py
+
+import config
+
+WHITELIST = set('0123456789abcdefghijklmnopqrstuvwxyz')
 VOCAB_SIZE = 1200
 UNK = 'unk'
 
 limit = {
-    'max_descriptions' : 400,
-    'min_descriptions' : 0,
-    'max_headings' : 20,
-    'min_headings' : 0,
+    'max_descriptions': 400,
+    'min_descriptions': 0,
+    'max_headings': 20,
+    'min_headings': 0,
 }
 
-def load_raw_data(filename):
-
+def load_raw_data(filename: str) -> List[Dict]:
+    """Load raw data from a JSON file."""
     with open(filename, 'r') as fp:
         raw_data = json.load(fp)
-
-    print('Loaded {:,} articles from {}'.format(len(raw_data), filename))
+    
+    print(f'Loaded {len(raw_data):,} articles from {filename}')
     return raw_data
 
-def tokenize_sentence(sentence):
+def tokenize_sentence(sentence: str) -> str:
+    """Tokenize a sentence using NLTK."""
+    return ' '.join(word_tokenize(sentence))
 
-    return ' '.join(list(tokenize(sentence)))
+def article_is_complete(article: Dict) -> bool:
+    """Check if an article has both heading and description."""
+    return ('abstract' in article and 'article' in article 
+            and article['abstract'] is not None and article['article'] is not None)
 
-def article_is_complete(article):
-
-    # Check if an article has both heading and description
-
-    if ('abstract' not in article) or ('article' not in article):
-        return False
-    if (article['abstract'] is None) or (article['article'] is None):
-        return False
-
-    return True
-
-def tokenize_articles(raw_data):
-
-    # Tokenizes data and creates list of headings and descriptions
-
+def tokenize_articles(raw_data: List[Dict]) -> Tuple[List[str], List[str]]:
+    """Tokenize articles and create lists of headings and descriptions."""
     headings, descriptions = [], []
-    num_articles = len(raw_data)
-
+    
     for i, a in enumerate(raw_data):
         if article_is_complete(a):
             headings.append(tokenize_sentence(a['abstract']))
             descriptions.append(tokenize_sentence(a['article']))
         if i % config.print_freq == 0:
-            print('Tokenized {:,} / {:,} articles'.format(i, num_articles))
+            print(f'Tokenized {i:,} / {len(raw_data):,} articles')
+    
+    return headings, descriptions
 
-    return (headings, descriptions)
+def filter_text(text: str) -> str:
+    """Filter out characters not in whitelist."""
+    return ''.join(ch for ch in text if ch in WHITELIST)
 
-def filter(line, whitelist):
-
-    #   Filters out all characters which are not in whitelist
-
-    return ''.join([ch for ch in line if ch in whitelist])
-
-def filter_length(headings, descriptions):
-
+def filter_length(headings: List[str], descriptions: List[str]) -> Tuple[List[str], List[str]]:
+    """Filter articles based on length constraints."""
     if len(headings) != len(descriptions):
-        raise Exception('Number of headings does not match number of descriptions!')
+        raise ValueError('Number of headings does not match number of descriptions!')
 
-    filtered_headings, filtered_descriptions = [], []
+    filtered_data = [
+        (h, d) for h, d in zip(headings, descriptions)
+        if (limit['min_descriptions'] <= len(d.split()) <= limit['max_descriptions'] and
+            limit['min_headings'] <= len(h.split()) <= limit['max_headings'])
+    ]
 
-    for i in range(0, len(headings)):
-        heading_length = len(headings[i].split(' '))
-        description_length = len(descriptions[i].split(' '))
+    filtered_headings, filtered_descriptions = zip(*filtered_data)
+    
+    print(f'Length of filtered headings: {len(filtered_headings):,}')
+    print(f'Length of filtered descriptions: {len(filtered_descriptions):,}')
 
-        if description_length >= limit['min_descriptions'] and description_length <= limit['max_descriptions']:
-            if heading_length >= limit['min_headings'] and heading_length <= limit['max_headings']:
-                filtered_headings.append(headings[i])
-                filtered_descriptions.append(descriptions[i])
+    return list(filtered_headings), list(filtered_descriptions)
 
-    print('Length of filtered headings: {:,}'.format(len(filtered_headings)))
-    print('Length of filtered descriptions: {:,}'.format(len(filtered_descriptions)))
-
-    return (filtered_headings, filtered_descriptions)
-
-def index_data(tokenized_sentences, vocab_size):
-
-    # Forms vocab, idx2word and word2idx dicts
-
+def index_data(tokenized_sentences: List[List[str]], vocab_size: int) -> Tuple[List[str], Dict[str, int], nltk.FreqDist]:
+    """Form vocabulary, idx2word and word2idx dictionaries."""
     freq_dist = nltk.FreqDist(itertools.chain(*tokenized_sentences))
     vocab = freq_dist.most_common(vocab_size)
-    print('Vocab length: {:,}'.format(len(vocab)))
+    print(f'Vocab length: {len(vocab):,}')
 
-    idx2word = ['_'] + [UNK] + [x[0] for x in vocab]
-    word2idx = dict([(w, i) for i, w in enumerate(idx2word)])
+    idx2word = ['_', UNK] + [x[0] for x in vocab]
+    word2idx = {w: i for i, w in enumerate(idx2word)}
 
-    return (idx2word, word2idx, freq_dist)
+    return idx2word, word2idx, freq_dist
 
-def pad_seq(seq, lookup, max_length):
+def pad_seq(seq: List[str], lookup: Dict[str, int], max_length: int) -> List[int]:
+    """Pad sequence with zero values."""
+    indices = [lookup.get(word, lookup[UNK]) for word in seq]
+    return indices + [0] * (max_length - len(seq))
 
-    # Pads sequence with zero values
-
-    indices = []
-
-    for word in seq:
-        if word in lookup:
-            indices.append(lookup[word])
-        else:
-            indices.append(lookup[UNK])
-
-    return indices + [0]*(max_length - len(seq))
-
-def zero_pad(tokenized_headings, tokenized_descriptions, word2idx):
-
-    # Stores indices in numpy arrays and
-    # Creates zero padding where required
-
+def zero_pad(tokenized_headings: List[List[str]], tokenized_descriptions: List[List[str]], word2idx: Dict[str, int]) -> Tuple[np.ndarray, np.ndarray]:
+    """Store indices in numpy arrays and create zero padding where required."""
     data_length = len(tokenized_descriptions)
 
-    idx_descriptions = np.zeros([data_length, limit['max_descriptions']], dtype=np.int32)
-    idx_headings = np.zeros([data_length, limit['max_headings']], dtype=np.int32)
+    idx_descriptions = np.zeros((data_length, limit['max_descriptions']), dtype=np.int32)
+    idx_headings = np.zeros((data_length, limit['max_headings']), dtype=np.int32)
 
-    for i in range(data_length):
-        description_indices = pad_seq(tokenized_descriptions[i], word2idx, limit['max_descriptions'])
-        heading_indices = pad_seq(tokenized_headings[i], word2idx, limit['max_headings'])
+    for i, (heading, description) in enumerate(zip(tokenized_headings, tokenized_descriptions)):
+        idx_descriptions[i] = pad_seq(description, word2idx, limit['max_descriptions'])
+        idx_headings[i] = pad_seq(heading, word2idx, limit['max_headings'])
 
-        idx_descriptions[i] = np.array(description_indices)
-        idx_headings[i] = np.array(heading_indices)
+    return idx_headings, idx_descriptions
 
-    return (idx_headings, idx_descriptions)
+def load_pretrained_embeddings(word2idx: Dict[str, int], embedding_dim: int = 300) -> np.ndarray:
+    """Load pre-trained word embeddings."""
+    model = KeyedVectors.load_word2vec_format('path_to_pretrained_embeddings', binary=True)
+    embedding_matrix = np.zeros((len(word2idx), embedding_dim))
+    
+    for word, i in word2idx.items():
+        if word in model.key_to_index:
+            embedding_matrix[i] = model[word]
+    
+    return embedding_matrix
+
+def augment_data(descriptions: List[str]) -> List[str]:
+    """Perform data augmentation on descriptions."""
+    augmented_descriptions = []
+    for description in descriptions:
+        augmented_descriptions.append(description)
+        # Add simple augmentation techniques
+        augmented_descriptions.append(' '.join(np.random.permutation(description.split())))
+        augmented_descriptions.append(' '.join(description.split()[::-1]))
+    return augmented_descriptions
 
 def process_data():
-
-    #load data from file
+    """Process the data and prepare it for model training."""
     filename = path.join(config.path_data, 'raw_data.json')
     raw_data = load_raw_data(filename)
 
-    #tokenize articles and separate into headings and descriptions
     headings, descriptions = tokenize_articles(raw_data)
 
-    #keep only whitelisted characters and articles satisfying the length limits
-    headings = [filter(heading, WHITELIST) for heading in headings]
-    descriptions = [filter(sentence, WHITELIST) for sentence in descriptions]
+    headings = [filter_text(heading) for heading in headings]
+    descriptions = [filter_text(sentence) for sentence in descriptions]
     headings, descriptions = filter_length(headings, descriptions)
 
-    #convert list of sentences into list of list of words
-    word_tokenized_headings = [word_list.split(' ') for word_list in headings]
-    word_tokenized_descriptions = [word_list.split(' ') for word_list in descriptions]
+    # Data augmentation
+    augmented_descriptions = augment_data(descriptions)
+    
+    word_tokenized_headings = [word_list.split() for word_list in headings]
+    word_tokenized_descriptions = [word_list.split() for word_list in augmented_descriptions]
 
-    #indexing
     idx2word, word2idx, freq_dist = index_data(word_tokenized_headings + word_tokenized_descriptions, VOCAB_SIZE)
 
-    #save as numpy array and do zero padding
     idx_headings, idx_descriptions = zero_pad(word_tokenized_headings, word_tokenized_descriptions, word2idx)
 
-    #check percentage of unks
     unk_percentage = calculate_unk_percentage(idx_headings, idx_descriptions, word2idx)
-    print(calculate_unk_percentage(idx_headings, idx_descriptions, word2idx))
+    print(f"UNK percentage: {unk_percentage:.2f}%")
+
+    # Load pre-trained embeddings
+    embedding_matrix = load_pretrained_embeddings(word2idx)
 
     article_data = {
-        'word2idx' : word2idx,
+        'word2idx': word2idx,
         'idx2word': idx2word,
         'limit': limit,
         'freq_dist': freq_dist,
+        'embedding_matrix': embedding_matrix
     }
 
-    pickle_data(article_data)
+    save_data(article_data, idx_headings, idx_descriptions)
 
-    return (idx_headings, idx_descriptions)
+    return idx_headings, idx_descriptions
 
-def pickle_data(article_data):
+def save_data(article_data: Dict, idx_headings: np.ndarray, idx_descriptions: np.ndarray):
+    """Save processed data to disk using HDF5 format."""
+    with h5py.File(path.join(config.path_data, 'processed_data.h5'), 'w') as hf:
+        hf.create_dataset('idx_headings', data=idx_headings)
+        hf.create_dataset('idx_descriptions', data=idx_descriptions)
+        hf.create_dataset('embedding_matrix', data=article_data['embedding_matrix'])
+        
+        # Save metadata
+        metadata = hf.create_group('metadata')
+        metadata.attrs['vocab_size'] = len(article_data['word2idx'])
+        metadata.attrs['max_heading_length'] = limit['max_headings']
+        metadata.attrs['max_description_length'] = limit['max_descriptions']
 
-    # Saves obj to disk as a pickle file
+    # Save other data using pickle
+    with open(path.join(config.path_data, 'article_metadata.pkl'), 'wb') as fp:
+        pickle.dump({k: v for k, v in article_data.items() if k != 'embedding_matrix'}, fp)
 
-    with open(path.join(config.path_data, 'article_data.pkl'), 'wb') as fp:
-        pickle.dump(article_data, fp, 2)
+def load_processed_data() -> Tuple[Dict, np.ndarray, np.ndarray]:
+    """Load processed data from disk."""
+    with h5py.File(path.join(config.path_data, 'processed_data.h5'), 'r') as hf:
+        idx_headings = hf['idx_headings'][:]
+        idx_descriptions = hf['idx_descriptions'][:]
+        embedding_matrix = hf['embedding_matrix'][:]
 
-def unpickle_articles():
-
-    #   Loads pickle file from disk to give obj
-
-    with open(path.join(config.path_data, 'article_data.pkl'), 'rb') as fp:
+    with open(path.join(config.path_data, 'article_metadata.pkl'), 'rb') as fp:
         article_data = pickle.load(fp)
+    
+    article_data['embedding_matrix'] = embedding_matrix
+    return article_data, idx_headings, idx_descriptions
 
-    return article_data
-
-def calculate_unk_percentage(idx_headings, idx_descriptions, word2idx):
-    num_unk = (idx_headings == word2idx[UNK]).sum() + (idx_descriptions == word2idx[UNK]).sum()
-    num_words = (idx_headings > word2idx[UNK]).sum() + (idx_descriptions > word2idx[UNK]).sum()
-
+def calculate_unk_percentage(idx_headings: np.ndarray, idx_descriptions: np.ndarray, word2idx: Dict[str, int]) -> float:
+    """Calculate the percentage of unknown words in the dataset."""
+    num_unk = np.sum(idx_headings == word2idx[UNK]) + np.sum(idx_descriptions == word2idx[UNK])
+    num_words = np.sum(idx_headings > word2idx[UNK]) + np.sum(idx_descriptions > word2idx[UNK])
     return (num_unk / num_words) * 100
 
 def main():
